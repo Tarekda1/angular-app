0965223fc3a1624642a73719f6491515ea378b86
tdaaboul86 <t.daaboul@totalcare.us>
2025-02-04 17:10:06 +0200

    main

--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -4,17 +4,19 @@ IMAGE_NAME="${DOCKER_IMAGE}" # Or specify it here
 IMAGE_TAG="${DOCKER_TAG}"
 
 # 1. Build for amd64 (your staging server's architecture)
-docker build -t "${IMAGE_NAME}:${IMAGE_TAG}-amd64" .
+docker build --platform linux/amd64  -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 
 # 2. Build for other architectures if needed (e.g., arm64)
 # docker build -t "${IMAGE_NAME}:${IMAGE_TAG}-arm64" .
 
 # 3. Create and push the manifest (important!)
-docker manifest create "${IMAGE_NAME}:${IMAGE_TAG}" \
-    --amend "${IMAGE_NAME}:${IMAGE_TAG}-amd64" \
+#docker manifest create "${IMAGE_NAME}:${IMAGE_TAG}" \
+#    --amend "${IMAGE_NAME}:${IMAGE_TAG}-amd64" \
     # --amend "${IMAGE_NAME}:${IMAGE_TAG}-arm64"  # Uncomment if you built for arm64
 
-docker manifest push "${IMAGE_NAME}:${IMAGE_TAG}"
+#docker manifest push "${IMAGE_NAME}:${IMAGE_TAG}"
+
+docker  push "${IMAGE_NAME}:${IMAGE_TAG}"
 
 # 4. (Optional) Inspect the manifest
 docker manifest inspect "${IMAGE_NAME}:${IMAGE_TAG}"
